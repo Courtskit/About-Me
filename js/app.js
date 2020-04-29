b8124430429b926115668c955e1187018f0f94cb
Courtskit <Courtskit@gmail.com>
2020-04-28 18:27:24 -0700

    JS UPDATED WITH ABOUT ME PROMPTS

--- js/app.js
+++ js/app.js
@@ -1,11 +1,60 @@
 'use strict';
-var doYou = prompt('Do you know');
-if (doYou.toLowerCase() === 'yes') {
-  alert('That is great!');
-} else if (doYou.toLowerCase() === 'no') {
-  alert('Oh, well that is okay.');
-} else {
-  prompt('Please answer with a yes or no.');
-}
 
 var userName = prompt('What is your name?');
+alert('Welcome to my page ' + userName + '! To begin, lets play a little game so that you can get to know me better.');
+
+
+//Asks user if I like to travel
+var likeToTravel = prompt('Gonna start this off easy for you. I work as a flight attendant. Do you think I like to travel?').toLowerCase();
+if (likeToTravel === 'yes' || likeToTravel === 'y') {
+  alert('Nice job ' + userName + '!');
+  // console.log(userName + ' answered correctly.');
+}
+if (likeToTravel === 'no' || likeToTravel === 'n') {
+  alert('Well sitting on a plane does get old but I still love to travel!');
+  // console.log(userName + ' answered incorrectly.');
+};
+
+// Asks user if I like math
+var likeMath = prompt('This question may be a bit trickier for yuh. Do you think that I enjoy math?').toLowerCase();
+if (likeMath === 'yes' || likeMath === 'y') {
+  alert('I do! I love problem solving.');
+  // console.log(userName + ' answered correctly.');
+}
+if (likeMath === 'no' || likeMath === 'n') {
+  alert('Actually, I really enjoy math!');
+  // console.log(userName + ' answered incorrectly.');
+};
+
+// Asks user if I like to run
+var likeToRun = prompt('I also enjoy eating healthy and working out. Do you think that I like to run?').toLowerCase();
+if (likeToRun === 'yes' || likeToRun === 'y') {
+  alert('Absolutly not! Running is so hard for me. Good guess though.');
+  // console.log(userName + ' answered incorrectly.');
+}
+if (likeToRun === 'no' || likeToRun === 'n') {
+  alert('You are right! I do not enjoy running.');
+  // console.log(userName + ' answered correctly.');
+};
+
+// Asks user if they think I like coffee
+var likeCoffee = prompt('Would you guess that I like coffee?').toLowerCase();
+if (likeCoffee === 'yes' || likeCoffee === 'y') {
+  alert('Good answer, I LOVE coffee! ');
+  // console.log(userName + ' answered correctly.');
+}
+if (likeCoffee === 'no' || likeCoffee === 'n') {
+  alert('How could you say no to this question. I am disappointed, I thought you knew me better than that.');
+  // console.log(userName + ' answered incorrectly.');
+};
+
+// Asks user if they think I am from the West Coast
+var fromWestCoast = prompt('Do you think I am from the West Coast?').toLowerCase();
+if (fromWestCoast === 'yes' || fromWestCoast === 'y') {
+  alert('Nope! I am from South Carolina but I do love it here in the Pacific North West.');
+  // console.log(userName + ' answered incorrectly.');
+}
+if (fromWestCoast === 'no' || fromWestCoast === 'n') {
+  alert(userName + ', how did you know? I am from South Carolina!');
+  // console.log(userName + ' answered correctly.');
+};
