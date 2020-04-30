afd5a21f429098a230f663714dff61f745bc6ec4
Courtskit <Courtskit@gmail.com>
2020-04-29 18:28:56 -0700

    ADD GUESS NUMBER GAME AND FAV COLOR JS

--- js/app.js
+++ js/app.js
@@ -1,68 +1,112 @@
-// 'use strict';
+'use strict';
 
-// var userName = prompt('What is your name?');
-// alert('Welcome to my page ' + userName + '! To begin, lets play a little game so that you can get to know me better.');
+var userName = prompt('What is your name?');
+alert('Welcome to my page ' + userName + '! To begin, lets play a little game so that you can get to know me better.');
 
+var score = 0;
 
-// //Asks user if I like to travel
-// var likeToTravel = prompt('Gonna start this off easy for you. I work as a flight attendant. Do you think I like to travel?').toLowerCase();
-// if (likeToTravel === 'yes' || likeToTravel === 'y') {
-//   alert('Nice job ' + userName + '!');
-//   // console.log(userName + ' answered correctly.');
-// }
-// if (likeToTravel === 'no' || likeToTravel === 'n') {
-//   alert('Well sitting on a plane does get old but I still love to travel!');
-//   // console.log(userName + ' answered incorrectly.');
-// }
-
-
-// // Asks user if I like math
-// var likeMath = prompt('This question may be a bit trickier for yuh. Do you think that I enjoy math?').toLowerCase();
-// if (likeMath === 'yes' || likeMath === 'y') {
-//   alert('I do! I love problem solving.');
-//   // console.log(userName + ' answered correctly.');
-// }
-// if (likeMath === 'no' || likeMath === 'n') {
-//   alert('Actually, I really enjoy math!');
-//   // console.log(userName + ' answered incorrectly.');
-// }
+//Asks user if I like to travel
+var likeToTravel = prompt('Gonna start this off easy for you. I work as a flight attendant. Do you think I like to travel?').toLowerCase();
+if (likeToTravel === 'yes' || likeToTravel === 'y') {
+  alert('Nice job ' + userName + '!');
+  score++;
+  // console.log(userName + ' answered correctly.');
+}
+if (likeToTravel === 'no' || likeToTravel === 'n') {
+  alert('Well sitting on a plane does get old but I still love to travel!');
+  // console.log(userName + ' answered incorrectly.');
+}
 
 
-// // Asks user if I like to run
-// var likeToRun = prompt('I also enjoy eating healthy and working out. Do you think that I like to run?').toLowerCase();
-// if (likeToRun === 'yes' || likeToRun === 'y') {
-//   alert('Absolutly not! Running is so hard for me. Good guess though.');
-//   // console.log(userName + ' answered incorrectly.');
-// }
-// if (likeToRun === 'no' || likeToRun === 'n') {
-//   alert('You are right! I do not enjoy running.');
-//   // console.log(userName + ' answered correctly.');
-// }
+// Asks user if I like math
+var likeMath = prompt('This question may be a bit trickier for yuh. Do you think that I enjoy math?').toLowerCase();
+if (likeMath === 'yes' || likeMath === 'y') {
+  alert('I do! I love problem solving.');
+  score++;
+  // console.log(userName + ' answered correctly.');
+}
+if (likeMath === 'no' || likeMath === 'n') {
+  alert('Actually, I really enjoy math!');
+  // console.log(userName + ' answered incorrectly.');
+}
 
 
-// // Asks user if they think I like coffee
-// var likeCoffee = prompt('Would you guess that I like coffee?').toLowerCase();
-// if (likeCoffee === 'yes' || likeCoffee === 'y') {
-//   alert('Good answer, I LOVE coffee! ');
-//   // console.log(userName + ' answered correctly.');
-// }
-// if (likeCoffee === 'no' || likeCoffee === 'n') {
-//   alert('How could you say no to this question. I am disappointed, I thought you knew me better than that.');
-//   // console.log(userName + ' answered incorrectly.');
-// }
+// Asks user if I like to run
+var likeToRun = prompt('I also enjoy eating healthy and working out. Do you think that I like to run?').toLowerCase();
+if (likeToRun === 'yes' || likeToRun === 'y') {
+  alert('Absolutly not! Running is so hard for me. Good guess though.');
+  // console.log(userName + ' answered incorrectly.');
+}
+if (likeToRun === 'no' || likeToRun === 'n') {
+  alert('You are right! I do not enjoy running.');
+  score++;
+  // console.log(userName + ' answered correctly.');
+}
 
 
-// // Asks user if they think I am from the West Coast
-// var fromWestCoast = prompt('Do you think I am from the West Coast?').toLowerCase();
-// if (fromWestCoast === 'yes' || fromWestCoast === 'y') {
-//   alert('Nope! I am from South Carolina but I do love it here in the Pacific North West.');
-//   // console.log(userName + ' answered incorrectly.');
-// }
-// if (fromWestCoast === 'no' || fromWestCoast === 'n') {
-//   alert(userName + ', how did you know? I am from South Carolina!');
-//   // console.log(userName + ' answered correctly.');
-// }
+// Asks user if they think I like coffee
+var likeCoffee = prompt('Would you guess that I like coffee?').toLowerCase();
+if (likeCoffee === 'yes' || likeCoffee === 'y') {
+  alert('Good answer, I LOVE coffee! ');
+  score++;
+  // console.log(userName + ' answered correctly.');
+}
+if (likeCoffee === 'no' || likeCoffee === 'n') {
+  alert('How could you say no to this question. I am disappointed, I thought you knew me better than that.');
+  // console.log(userName + ' answered incorrectly.');
+}
 
 
-// alert('I hope enjoyed learning a few facts about me ' + userName + '. Thanks for visiting my site.');
+// Asks user if they think I am from the West Coast
+var fromWestCoast = prompt('Do you think I am from the West Coast?').toLowerCase();
+if (fromWestCoast === 'yes' || fromWestCoast === 'y') {
+  alert('Nope! I am from South Carolina but I do love it here in the Pacific North West.');
+  // console.log(userName + ' answered incorrectly.');
+}
+if (fromWestCoast === 'no' || fromWestCoast === 'n') {
+  alert(userName + ', how did you know? I am from South Carolina!');
+  score++;
+  // console.log(userName + ' answered correctly.');
+}
+
+var numberGame = 4;
+for (var i = 0; i < 4; i++) {
+  var guess = prompt('Guess my favorite number from 1-10?');
+  if (guess == numberGame) {
+    alert('You are correct! Great guess.');
+    score++;
+    break;
+  } else if (guess < numberGame) {
+    alert('Wrong. You are too low!');
+  } else if (guess > numberGame) {
+    alert('Wrong. You are too high!');
+  }
+  if (i === 3) {
+    alert('You lost. The correct answer is 4.');
+  }
+}
+
+var favColor = ['blue', 'green', 'yellow'];
+
+for (var i = 0; i < 6; i++) {
+  var colorGuess = prompt('What do you think is one of my favorite colors?');
+  if (colorGuess === favColor[0] || colorGuess === favColor[1] || colorGuess === favColor[2]) {
+    console.log('You are right. Blue, green, and yellow are my favorite!');
+    score++;
+    break;
+  } else {
+    console.log('Nope.');
+  }
+  if (i === 5) {
+    alert('Good try. My favorite colors are blue, green, and yellow.');
+  }
+}
+
+
+
+
+
+
+alert('I hope enjoyed learning a few facts about me, ' + userName + '. You scored ' + score + ' out of 7. Thanks for visiting my site!');
+
 
